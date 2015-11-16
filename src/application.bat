bc8e1d1da1d7b2f204f8449b3c68a44da51a70c8
Nicolas Rauflet <nicolas.rauflet@tactfactory.com>
2015-11-16 12:05:55 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
 ### application.bat - 201x - Nicolas Rauflet ###
-echo "Copyright Nicolas Rauflet - 201x"
-echo "Build : 10:00"
-echo "version 1.1"
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
